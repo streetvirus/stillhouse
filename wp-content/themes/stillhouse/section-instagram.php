272b71505c57c04d583a432a8d22c03d39d1b2b6
Stefan Bowerman <stefbowerman@gmail.com>
2015-02-09 10:47:29 -0800

    Changed the instagram pics out for hard coded links

--- wp-content/themes/stillhouse/section-instagram.php
+++ wp-content/themes/stillhouse/section-instagram.php
@@ -9,7 +9,46 @@
     <div class="row">
       <div class="instagram-gallery-container col-xs-12">
         <ul id="instagram-gallery" class="instagram-gallery">
-          
+          <li>
+            <a href="http://instagram.com/p/qfkPpBk-Dv" target="_blank">
+              <img src="http://photos-e.ak.instagram.com/hphotos-ak-xpa1/t51.2885-15/925574_760321124010572_358528441_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/sWTBL9E-J1" target="_blank">
+              <img src="http://scontent-b-lax.cdninstagram.com/hphotos-xap1/t51.2885-15/926556_948767998470520_610764988_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/qDT_q-E-PN" target="_blank">
+              <img src="http://photos-g.ak.instagram.com/hphotos-ak-xpf1/t51.2885-15/10488647_251654818374558_196037426_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/puwg_bk-NM" target="_blank">
+              <img src="http://photos-d.ak.instagram.com/hphotos-ak-xpa1/t51.2885-15/927082_1378962272384939_895866041_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/oC9tIGE-FR" target="_blank">
+              <img src="http://photos-f.ak.instagram.com/hphotos-ak-xfp1/t51.2885-15/10354541_310165495808925_672539598_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/um8gtHE-NR" target="_blank">
+              <img src="http://photos-g.ak.instagram.com/hphotos-ak-xap1/t51.2885-15/10724631_814544865276486_1565391552_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/uJVaviE-FH" target="_blank">
+              <img src="http://photos-a.ak.instagram.com/hphotos-ak-xfp1/t51.2885-15/10467988_305203793002048_25874005_n.jpg" />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com/p/oeyCOxk-P5" target="_blank">
+              <img src="http://photos-d.ak.instagram.com/hphotos-ak-xpa1/t51.2885-15/10354493_235927256602299_1406253545_n.jpg" />
+            </a>
+          </li>
         </ul>
       </div>
     </div>
